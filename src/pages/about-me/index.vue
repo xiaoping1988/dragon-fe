3ea99e6404f2f6614f9e109190dde6c3dd4e8362
liujiangping <liujiangping@daojia-inc.com>
2019-08-19 20:34:11 +0800

    aboutme

---
+++ src/pages/about-me/index.vue
@@ -0,0 +1,68 @@
+<template>
+    <DPageContent>
+        <el-row style="height: 100%">
+            <el-col class="d-col" style="height: 100%">
+                <el-card shadow="never" style="height: 100%">
+                    <div class="d-user">
+                        <img src="../../assets/me.jpg"/>
+                        <div class="d-user-name" style="margin-bottom: 20px">小平</div>
+                    </div>
+                    <div class="d-box-middle">
+                        <div class="d-me">
+                            <el-row class="d-row">
+                                <i class="el-icon-suitcase d-me-icon"></i> <span>我就是个敲代码的</span>
+                            </el-row>
+                            <el-row class="d-row">
+                                <i class="fa fa-wechat d-me-icon"></i><span>136291865</span>
+                            </el-row>
+                            <el-row class="d-row">
+                                <i class="el-icon-location-outline d-me-icon"></i><span>58到家</span>
+                            </el-row>
+                            <el-row class="d-row">
+                                <i class="fa fa-github d-me-icon"></i><a href="https://github.com/xiaoping1988" target="_blank">https://github.com/xiaoping1988</a>
+                            </el-row>
+                            <el-row class="d-row">
+                                <i class="fa fa-share-alt d-me-icon"></i><a href="https://blog.csdn.net/xiaoping_1988" target="_blank">https://blog.csdn.net/xiaoping_1988</a>
+                            </el-row>
+                        </div>
+                    </div>
+                </el-card>
+            </el-col>
+        </el-row>
+    </DPageContent>
+</template>
+
+<script>
+    export default {
+        name: 'DAboutMe'
+    }
+</script>
+
+<style>
+    .d-user {
+        text-align: center;
+    }
+
+    .d-user img {
+        width: 204px;
+        height: 204px;
+        margin-bottom: 20px;
+    }
+
+    .d-user-name {
+        font-size: 20px;
+        font-weight: 700;
+    }
+
+    .d-me {
+        font-size: 20px;
+        width: 454px;
+        padding-left: 100px;
+        box-sizing: border-box;
+    }
+    
+    .d-me-icon {
+        text-align: right;
+        padding-right: 15px;
+    }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,51 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Pages from '../pages'
+import Pages from '../pages/data-visual/chart-demo/index.vue'
+const Home = () => import('../pages/index.vue')
+const SSO = () => import('../pages/sso/index.vue')
+const Auth = () => import('../pages/auth/index.vue')
+const AuthResource = () => import('../pages/auth/resource/index.vue')
+const AuthRole = () => import('../pages/auth/role/index.vue')
+const AuthUser = () => import('../pages/auth/user/index.vue')
+const AuthConsole = () => import('../pages/auth/console/index.vue')
+const Search = () => import('../pages/search/index.vue')
+const Message = () => import('../pages/message/index.vue')
+const MessageOnsite = () => import('../pages/message/onsite/index.vue')
+const MessageReceiveManage = () => import('../pages/message/receive-manage/index.vue')
+const DataMap = () => import('../pages/data-map/index.vue')
+const DataMapMyTb = () => import('../pages/data-map/my-tb/index.vue')
+const DataMapMyTbOwn = () => import('../pages/data-map/my-tb/own/index.vue')
+const DataMapMyTbManage = () => import('../pages/data-map/my-tb/manage/index.vue')
+const DataMapMyTbAuth = () => import('../pages/data-map/my-tb/auth/index.vue')
+const DataMapDsManage = () => import('../pages/data-map/ds-manage/index.vue')
+const DataMapDsManageDetail = () => import('../pages/data-map/ds-manage/detail.vue')
+const DataMapTbManage = () => import('../pages/data-map/tb-manage/index.vue')
+const DataMapTbManageDetail = () => import('../pages/data-map/tb-manage/detail.vue')
+const DataMapDbManageDetail = () => import('../pages/data-map/db-manage/detail.vue')
+const DataVisual = () => import('../pages/data-visual/index.vue')
+const DataVisualDashboard = () => import('../pages/data-visual/dashboard/index.vue')
+const DataVisualDashboardContainer = () => import('../pages/data-visual/dashboard/container.vue')
+const DataVisualChartDemo = () => import('../pages/data-visual/chart-demo/index.vue')
+const DataVisualChartDemoIndexCard = () => import('../pages/data-visual/chart-demo/IndexCard.vue')
+const DataVisualChartDemoTable = () => import('../pages/data-visual/chart-demo/Table.vue')
+const DataVisualChartDemoLineBar = () => import('../pages/data-visual/chart-demo/LineBar.vue')
+const DataVisualChartDemoLineBarNotDate = () => import('../pages/data-visual/chart-demo/LineBarNotDate.vue')
+const DataVisualChartDemoLine = () => import('../pages/data-visual/chart-demo/Line.vue')
+const DataVisualChartDemoLineNotDate = () => import('../pages/data-visual/chart-demo/LineNotDate.vue')
+const DataVisualChartDemoBar = () => import('../pages/data-visual/chart-demo/Bar.vue')
+const DataVisualChartDemoBarStack = () => import('../pages/data-visual/chart-demo/BarStack.vue')
+const DataVisualChartDemoBarStackTop = () => import('../pages/data-visual/chart-demo/BarStackTop.vue')
+const DataVisualChartDemoBarTop = () => import('../pages/data-visual/chart-demo/BarTop.vue')
+const DataVisualChartDemoBarNotDate = () => import('../pages/data-visual/chart-demo/BarNotDate.vue')
+const DataVisualChartDemoArea = () => import('../pages/data-visual/chart-demo/Area.vue')
+const DataVisualChartDemoAreaNotDate = () => import('../pages/data-visual/chart-demo/AreaNotDate.vue')
+const DataVisualChartDemoPie = () => import('../pages/data-visual/chart-demo/Pie.vue')
+const DataVisualChartDemoPieRing = () => import('../pages/data-visual/chart-demo/PieRing.vue')
+const DataVisualChartDemoFunnel = () => import('../pages/data-visual/chart-demo/Funnel.vue')
+const DataVisualChartDemoFunnelDim = () => import('../pages/data-visual/chart-demo/FunnelDim.vue')
+const DataVisualChartDemoMap = () => import('../pages/data-visual/chart-demo/Map.vue')
+const DataVisualChartDemoChartFilter = () => import('../pages/data-visual/chart-demo/ChartFilter.vue')
+const AboutMe = () => import('../pages/about-me/index.vue')
 
 Vue.use(Router)
 
@@ -8,7 +53,7 @@ let router = new Router({
     routes: [
         {
             path: '/',
-            component: Pages.Home,
+            component: Home,
             redirect: '/data-visual'
         },
         {
@@ -17,88 +62,92 @@ let router = new Router({
         },
         {
             path: '/sso/login',
-            component: Pages.SSO
+            component: SSO
         },
         {
             path: '/auth',
-            component: Pages.Auth,
+            component: Auth,
             children: [
-                {path: 'resource', component: Pages.Auth.Resource},
-                {path: 'role', component: Pages.Auth.Role},
-                {path: 'user', component: Pages.Auth.User},
-                {path: 'console', component: Pages.Auth.Console}
+                {path: 'resource', component: AuthResource},
+                {path: 'role', component: AuthRole},
+                {path: 'user', component: AuthUser},
+                {path: 'console', component: AuthConsole}
             ],
             redirect: '/auth/console'
         },
         {
             path: '/search',
-            component: Pages.Search
+            component: Search
+        },
+        {
+            path: '/about-me',
+            component: AboutMe
         },
         {
             path: '/message',
-            component: Pages.Message,
+            component: Message,
             children: [
-                {path: 'onsite', component: Pages.Message.Onsite},
-                {path: 'receive-manage', component: Pages.Message.ReceiveManage}
+                {path: 'onsite', component: MessageOnsite},
+                {path: 'receive-manage', component: MessageReceiveManage}
             ],
             redirect: '/message/onsite'
         },
         {
             path: '/data-map',
-            component: Pages.DataMap,
+            component: DataMap,
             children: [
                 {
                     path: 'my-tb',
-                    component: Pages.DataMap.MyTb,
+                    component: DataMapMyTb,
                     children: [
-                        {path: 'own', component: Pages.DataMap.MyTb.Own},
-                        {path: 'manage', component: Pages.DataMap.MyTb.Manage},
-                        {path: 'auth', component: Pages.DataMap.MyTb.Auth}
+                        {path: 'own', component: DataMapMyTbOwn},
+                        {path: 'manage', component: DataMapMyTbManage},
+                        {path: 'auth', component: DataMapMyTbAuth}
                     ],
                     redirect: '/data-map/my-tb/own'
                 },
-                {path: 'ds-manage', component: Pages.DataMap.DsManager},
-                {path: 'ds-manage/:id', component: Pages.DataMap.DsManager.Detail},
-                {path: 'db-manage/:id', component: Pages.DataMap.DbManager.Detail},
-                {path: 'tb-manage', component: Pages.DataMap.TbManager},
-                {path: 'tb-manage/:id', component: Pages.DataMap.TbManager.Detail}
+                {path: 'ds-manage', component: DataMapDsManage},
+                {path: 'ds-manage/:id', component: DataMapDsManageDetail},
+                {path: 'db-manage/:id', component: DataMapDbManageDetail},
+                {path: 'tb-manage', component: DataMapTbManage},
+                {path: 'tb-manage/:id', component: DataMapTbManageDetail}
             ],
             redirect: '/data-map/my-tb/own'
         },
         {
             path: '/data-visual',
-            component: Pages.DataVisual,
+            component: DataVisual,
             children: [
                 {
                     path: 'dashboard',
-                    component: Pages.DataVisual.Dashboard,
+                    component: DataVisualDashboard,
                     children: [
-                        {path: ':projId/:dashId', component: Pages.DataVisual.Dashboard.Container}
+                        {path: ':projId/:dashId', component: DataVisualDashboardContainer}
                     ]
                 },
                 {
                     path: 'chart-demo',
-                    component: Pages.DataVisual.ChartDemo,
+                    component: DataVisualChartDemo,
                     children: [
-                        {path: 'indexcard', component: Pages.DataVisual.ChartDemo.IndexCard},
-                        {path: 'table', component: Pages.DataVisual.ChartDemo.Table},
-                        {path: 'line-bar', component: Pages.DataVisual.ChartDemo.LineBar},
-                        {path: 'line-bar-not-date', component: Pages.DataVisual.ChartDemo.LineBarNotDate},
-                        {path: 'line', component: Pages.DataVisual.ChartDemo.Line},
-                        {path: 'line-not-date', component: Pages.DataVisual.ChartDemo.LineNotDate},
-                        {path: 'bar', component: Pages.DataVisual.ChartDemo.Bar},
-                        {path: 'bar-stack', component: Pages.DataVisual.ChartDemo.BarStack},
-                        {path: 'bar-stack-top', component: Pages.DataVisual.ChartDemo.BarStackTop},
-                        {path: 'bar-top', component: Pages.DataVisual.ChartDemo.BarTop},
-                        {path: 'bar-not-date', component: Pages.DataVisual.ChartDemo.BarNotDate},
-                        {path: 'area', component: Pages.DataVisual.ChartDemo.Area},
-                        {path: 'area-not-date', component: Pages.DataVisual.ChartDemo.AreaNotDate},
-                        {path: 'pie', component: Pages.DataVisual.ChartDemo.Pie},
-                        {path: 'pie-ring', component: Pages.DataVisual.ChartDemo.PieRing},
-                        {path: 'funnel', component: Pages.DataVisual.ChartDemo.Funnel},
-                        {path: 'funnel-dim', component: Pages.DataVisual.ChartDemo.FunnelDim},
-                        {path: 'map', component: Pages.DataVisual.ChartDemo.Map},
-                        {path: 'filter', component: Pages.DataVisual.ChartDemo.ChartFilter}
+                        {path: 'indexcard', component: DataVisualChartDemoIndexCard},
+                        {path: 'table', component: DataVisualChartDemoTable},
+                        {path: 'line-bar', component: DataVisualChartDemoLineBar},
+                        {path: 'line-bar-not-date', component: DataVisualChartDemoLineBarNotDate},
+                        {path: 'line', component: DataVisualChartDemoLine},
+                        {path: 'line-not-date', component: DataVisualChartDemoLineNotDate},
+                        {path: 'bar', component: DataVisualChartDemoBar},
+                        {path: 'bar-stack', component: DataVisualChartDemoBarStack},
+                        {path: 'bar-stack-top', component: DataVisualChartDemoBarStackTop},
+                        {path: 'bar-top', component: DataVisualChartDemoBarTop},
+                        {path: 'bar-not-date', component: DataVisualChartDemoBarNotDate},
+                        {path: 'area', component: DataVisualChartDemoArea},
+                        {path: 'area-not-date', component: DataVisualChartDemoAreaNotDate},
+                        {path: 'pie', component: DataVisualChartDemoPie},
+                        {path: 'pie-ring', component: DataVisualChartDemoPieRing},
+                        {path: 'funnel', component: DataVisualChartDemoFunnel},
+                        {path: 'funnel-dim', component: DataVisualChartDemoFunnelDim},
+                        {path: 'map', component: DataVisualChartDemoMap},
+                        {path: 'filter', component: DataVisualChartDemoChartFilter}
                     ],
                     redirect: '/data-visual/chart-demo/indexcard'
                 }
