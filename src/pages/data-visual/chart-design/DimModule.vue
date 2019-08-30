c6d890084323808c63242376c817c4d6c546356c
liujiangping <liujiangping@daojia-inc.com>
2019-08-30 23:19:00 +0800

    批量添加字段

--- src/pages/data-visual/chart-design/DimModule.vue
+++ src/pages/data-visual/chart-design/DimModule.vue
@@ -63,15 +63,40 @@
                            submitText="确定">
             </DSubmitCancel>
         </el-dialog>
-        <el-dialog :visible.sync="batchAddModalVisible"
+        <el-dialog :title="'批量添加' + (batchAddTargetType === 0 ? '维度' : '对比') + '字段'"
+                   :visible.sync="batchAddModalVisible"
                    :close-on-click-modal="false"
-                   :close-on-press-escape="false" width="400px">
-
+                   :close-on-press-escape="false"
+                   width="600px">
+            <el-row  class="d-row">
+                <el-col :span="8">
+                    <el-input suffix-icon="el-icon-search"
+                              size="small"
+                              clearable
+                              v-model.trim="searchColKey"
+                              placeholder="请输入关键字搜索"></el-input>
+                </el-col>
+            </el-row>
+            <el-row v-for="(item, i) in searchedColList" :key="i"  class="d-row">
+                <el-col :span="5">
+                    <el-checkbox v-model="dataTypeChecked[item.code]" @change="(value) => changeDataTypeChecked(value, item)">
+                        <i class="fa d-icon" :class="'fa-' + item.icon"></i><span>{{item.name}}</span>
+                    </el-checkbox>
+                </el-col>
+                <el-col :span="19">
+                    <el-checkbox-group v-model="checkedColList"
+                                       @change="(value) => {changeCheckedCol(value, item)}">
+                        <el-checkbox v-for="(city, j) in item.colList"
+                                     :key="j"
+                                     :label="city.colName">{{city.colLabel}}</el-checkbox>
+                    </el-checkbox-group>
+                </el-col>
+            </el-row>
             <DSubmitCancel slot="footer"
                            nohr
                            size="mini"
-                           @submit="submitConfigForm"
-                           @cancel="configModalVisible = false"
+                           @submit="submitBatchAdd"
+                           @cancel="batchAddModalVisible = false"
                            submitText="确定">
             </DSubmitCancel>
         </el-dialog>
@@ -81,6 +106,7 @@
 <script>
     import {TimeFreq} from '../constants'
     import DDimDragContainer from './DimDragContainer'
+    import {DataType} from '../../../services/data-map/col-manage'
     export default {
         name: 'DDimModule',
         components: {DDimDragContainer},
@@ -108,7 +134,15 @@
                 activeConfigTab: '',
                 batchAddModalVisible: false, // 批量添加的窗口
                 batchAddTargetType: 0, // 批量添加的容器对象,维度0对比1
-                allColList: []
+                allColList: [
+                    {colName: 'id', colLabel: '订单ID', dataType: DataType.text.code},
+                    {colName: 'date', colLabel: '创建时间', dataType: DataType.date.code},
+                    {colName: 'amt', colLabel: '订单金额', dataType: DataType.num.code}
+                ], // 所有原始字段
+                searchedColList: [], // 批量添加，搜索出来的字段
+                searchColKey: '', // 搜索关键字
+                checkedColList: [],
+                dataTypeChecked: {}
             }
         },
         methods: {
@@ -146,8 +180,115 @@
             changeDimStore () {
 
             },
-            openBatchAdd () {
-
+            openBatchAdd (batchAddTargetType) {
+                this.batchAddModalVisible = true
+                this.batchAddTargetType = batchAddTargetType
+                this.checkedColList = []
+                this.dataTypeChecked = {}
+                this.searchColKey = ''
+                this.searchColList()
+            },
+            submitBatchAdd () {
+                this.checkedColList.forEach(colName => {
+                    let col = this.allColList.filter(c => c.colName === colName)[0]
+                    let tmpCol = JSON.parse(JSON.stringify(col))
+                    tmpCol.dimConfig = {
+                        key: tmpCol.colName + '_' + new Date().getTime(),
+                        showName: tmpCol.colLabel,
+                        sortType: '0',
+                        timeFreq: tmpCol.dataType === DataType.date.code ? TimeFreq.day.code : ''
+                    }
+                    if (this.batchAddTargetType === 0) { // 维度
+                        this.dimList.push(tmpCol)
+                    } else { // 对比
+                        this.contrastDimList.push(tmpCol)
+                    }
+                })
+                this.batchAddModalVisible = false
+            },
+            searchColList () {
+                let dateList = [] // 日期
+                let textList = [] // 文本
+                let numberList = [] // 数值
+                let tmpColList = []
+                if (this.searchColKey) {
+                    tmpColList = this.allColList.filter(c => c.colName.includes(this.searchColKey.toLowerCase()) || c.colLabel.includes(this.searchColKey))
+                } else {
+                    tmpColList = this.allColList
+                }
+                tmpColList.forEach(c => {
+                    if (c.dataType === DataType.date.code) {
+                        dateList.push(c)
+                    } else if (c.dataType === DataType.num.code) {
+                        numberList.push(c)
+                    } else {
+                        textList.push(c)
+                    }
+                })
+                let tmpSearchedColList = []
+                if (dateList.length) {
+                    tmpSearchedColList.push({
+                        code: DataType.date.code,
+                        name: '日期',
+                        icon: DataType.date.icon,
+                        colList: dateList
+                    })
+                }
+                if (textList.length) {
+                    tmpSearchedColList.push({
+                        code: DataType.text.code,
+                        name: '文本',
+                        icon: DataType.text.icon,
+                        colList: textList
+                    })
+                }
+                if (numberList.length) {
+                    tmpSearchedColList.push({
+                        code: DataType.num.code,
+                        name: '数值',
+                        icon: DataType.num.icon,
+                        colList: numberList
+                    })
+                }
+                this.searchedColList = tmpSearchedColList
+            },
+            setDataTypeChecked (dataType) {
+                this.dataTypeChecked[dataType.code] = true
+                for (let i in dataType.colList) {
+                    if (!this.checkedColList.includes(dataType.colList[i].colName)) {
+                        this.dataTypeChecked[dataType.code] = false
+                        break
+                    }
+                }
+            },
+            changeCheckedCol (value, dataType) {
+                this.setDataTypeChecked(dataType)
+                this.$forceUpdate()
+            },
+            changeDataTypeChecked (value, dataType) {
+                if (value) {
+                    dataType.colList.forEach(c => {
+                        if (!this.checkedColList.includes(c.colName)) {
+                            this.checkedColList.push(c.colName)
+                        }
+                    })
+                } else {
+                    let delIndex = []
+                    this.checkedColList.forEach((colName, i) => {
+                        let j = dataType.colList.findIndex(c => c.colName === colName)
+                        if (j >= 0) {
+                            delIndex.push(i)
+                        }
+                    })
+                    delIndex.forEach((d, i) => {
+                        this.checkedColList.splice(d - i, 1)
+                    })
+                }
+            }
+        },
+        watch: {
+            searchColKey () {
+                this.searchColList()
             }
         }
     }
